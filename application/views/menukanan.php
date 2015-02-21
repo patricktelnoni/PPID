67581b130692645dabe6e0ba76a12044adfb7b47
Patrick Telnoni <ptelnoni89@gmail.com>
2015-02-21 14:30:10 +0700

    PErbaiki variabel lama

---
+++ application/views/menukanan.php
@@ -0,0 +1,326 @@
+                  
+<td class="contentRight-1" rowspan="2">
+	<div class="contentRight">
+		<br/>           
+		<!-- AddThis Button BEGIN -->
+		<div style="padding-left:5px" class="addthis_toolbox addthis_default_style">
+		<a class="addthis_button_preferred_1"></a>
+		<a class="addthis_button_preferred_2"></a>
+		<a class="addthis_button_preferred_3"></a>
+		<a class="addthis_button_preferred_4"></a>
+		<span class="addthis_separator"> | </span>
+		<a href="http://www.addthis.com/bookmark.php?v=250&amp;username=xa-4cedd2793e237ec2" class="addthis_button_compact"></a>
+
+		<a href="http://www.facebook.com/perkantas.jawatimur" target="blank"><img style="border:none" src="images/find_us_on_facebook.gif" height="25" alt="Find Us on Facebook"></a>
+		<script>
+					function fbs_click() 
+					{
+						u=location.href;t=document.title;
+						window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(u)+'&t='+encodeURIComponent(t),'sharer','toolbar=0,status=0,width=626,height=436');return false;
+					}
+		</script>
+		&nbsp;
+		</div>
+		<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#username=xa-4cedd2793e237ec2"></script>
+
+<br>
+
+<div id="cse-search-form" style="width: 100%;">Loading</div>
+<script src="http://www.google.com/jsapi" type="text/javascript"></script>
+<script type="text/javascript"> 
+  google.load('search', '1', {language : 'en', style : google.loader.themes.GREENSKY});
+  google.setOnLoadCallback(function() {
+    var customSearchControl = new google.search.CustomSearchControl('008908821902838532206:tl-6tg9z924');
+    customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
+    var options = new google.search.DrawOptions();
+    options.setSearchFormRoot('cse-search-form');
+    customSearchControl.draw('cse', options);
+	//customSearchControl.draw('cse');
+  }, true);
+</script>
+ <style type="text/css">
+  .gsc-control-cse {
+    font-family: "Trebuchet MS", sans-serif;
+    border-color: transparent;
+    background-color: #B2C2FF;
+  }
+  input.gsc-input {
+    border-color: #94CC7A;
+  }
+  input.gsc-search-button {
+    border-color: #94CC7A;
+    background-color: #AADA92;
+  }
+  .gsc-tabHeader.gsc-tabhInactive {
+    border-color: #A9DA92;
+    background-color: #FFFFFF;
+  }
+  .gsc-tabHeader.gsc-tabhActive {
+    border-color: #A9DA92;
+    background-color: #A9DA92;
+  }
+  .gsc-tabsArea {
+    border-color: #A9DA92;
+  }
+  .gsc-webResult.gsc-result,
+  .gsc-results .gsc-imageResult {
+    border-color: #A9DA92;
+    background-color: #FFFFFF;
+  }
+  .gsc-webResult.gsc-result:hover,
+  .gsc-imageResult:hover {
+    border-color: #A9DA92;
+    background-color: #FFFFFF;
+  }
+  .gs-webResult.gs-result a.gs-title:link,
+  .gs-webResult.gs-result a.gs-title:link b,
+  .gs-imageResult a.gs-title:link,
+  .gs-imageResult a.gs-title:link b {
+    color: #0066CC;
+  }
+  .gs-webResult.gs-result a.gs-title:visited,
+  .gs-webResult.gs-result a.gs-title:visited b,
+  .gs-imageResult a.gs-title:visited,
+  .gs-imageResult a.gs-title:visited b {
+    color: #0066CC;
+  }
+  .gs-webResult.gs-result a.gs-title:hover,
+  .gs-webResult.gs-result a.gs-title:hover b,
+  .gs-imageResult a.gs-title:hover,
+  .gs-imageResult a.gs-title:hover b {
+    color: #0066CC;
+  }
+  .gs-webResult.gs-result a.gs-title:active,
+  .gs-webResult.gs-result a.gs-title:active b,
+  .gs-imageResult a.gs-title:active,
+  .gs-imageResult a.gs-title:active b {
+    color: #0066CC;
+  }
+  .gsc-cursor-page {
+    color: #0066CC;
+  }
+  a.gsc-trailing-more-results:link {
+    color: #0066CC;
+  }
+  .gs-webResult .gs-snippet,
+  .gs-imageResult .gs-snippet {
+    color: #454545;
+  }
+  .gs-webResult div.gs-visibleUrl,
+  .gs-imageResult div.gs-visibleUrl {
+    color: #815FA7;
+  }
+  .gs-webResult div.gs-visibleUrl-short {
+    color: #815FA7;
+  }
+  .gs-webResult div.gs-visibleUrl-short {
+    display: none;
+  }
+  .gs-webResult div.gs-visibleUrl-long {
+    display: block;
+  }
+  .gsc-cursor-box {
+    border-color: #A9DA92;
+  }
+  .gsc-results .gsc-cursor-box .gsc-cursor-page {
+    border-color: #A9DA92;
+    background-color: #FFFFFF;
+    color: #0066CC;
+  }
+  .gsc-results .gsc-cursor-box .gsc-cursor-current-page {
+    border-color: #A9DA92;
+    background-color: #A9DA92;
+    color: #0066CC;
+  }
+  .gs-promotion {
+    border-color: #94CC7A;
+    background-color: #B2C2FF;
+  }
+  .gs-promotion a.gs-title:link,
+  .gs-promotion a.gs-title:link *,
+  .gs-promotion .gs-snippet a:link {
+    color: #0066CC;
+  }
+  .gs-promotion a.gs-title:visited,
+  .gs-promotion a.gs-title:visited *,
+  .gs-promotion .gs-snippet a:visited {
+    color: #0066CC;
+  }
+  .gs-promotion a.gs-title:hover,
+  .gs-promotion a.gs-title:hover *,
+  .gs-promotion .gs-snippet a:hover {
+    color: #0066CC;
+  }
+  .gs-promotion a.gs-title:active,
+  .gs-promotion a.gs-title:active *,
+  .gs-promotion .gs-snippet a:active {
+    color: #0066CC;
+  }
+  .gs-promotion .gs-snippet,
+  .gs-promotion .gs-title .gs-promotion-title-right,
+  .gs-promotion .gs-title .gs-promotion-title-right *  {
+    color: #454545;
+  }
+  .gs-promotion .gs-visibleUrl,
+  .gs-promotion .gs-visibleUrl-short {
+    color: #815FA7;
+  }
+</style>
+
+		<a href="index.php?g=form_alumni"><img style="border:none" src="images/form_alumni.gif" width="241" height="60" alt="LENGKAPI DATA ANDA" /></a>
+		<br>
+		<br>
+		<a href="index.php?g=news_list"><img style="border:none" src="images/berita.jpg" width="241" height="30" alt="BERITA" /></a>
+        
+        
+		<div class="contentRight-3">			
+				
+					<a class="titleTextRight" href="index.php?g=news&id=" title=""?></a><br>
+						<span class="textRight">, Dibaca:  Komentar"?></span><br>
+						
+						
+						<div>
+						<img border="none" style="padding:5px 5px 1px 0" align="left" src="" width="50" height="34" alt="" />
+						</div>
+						
+						<span class="textLeft"> </span><br>
+						<div class="clearboth"></div>
+						<div style="height:5px"></div>
+										
+				<a class="titleTextRight" style="padding-left:90px" href="index.php?g=news_list" title="Lihat semua berita">Lihat Semua Berita >></a>										
+		</div>
+		
+		<div class="header_menukanan">
+		<a href="index.php?g=article_list"><img style="border:none" src="images/artikelbaru.jpg" width="241" alt="ARTIKEL TERBARU" /></a>
+        </div>  
+		  
+		<div class="contentRight-3">			
+				
+					<a class="titleTextRight" href="index.php?g=articles&id=" title=""></a><br>
+						<span class="textRight">oleh: </span><br>
+						<div style="height:5px"></div>
+				class="titleTextRight" style="padding-left:80px" href="index.php?g=article_list" title="Lihat semua artikel">Lihat Semua Artikel >></a>										
+		</div>
+		
+        
+		<!--<div class="header_menukanan"><img src="images/book.jpg" width="241" height="30" alt="BERITA" />
+        </div>
+		<div class="contentRight-3">			
+				<?php
+					$list = $book->getRightBook();
+					foreach ($list as $row)
+					{
+				?>
+					<a class="titleTextRight" href="index.php?g=book&id=<?=$row['bookId']?>" title="<?=$row['title']?>"><?=substr($row['title'],0,28)."..."?></a><br>
+						<span class="textRight">oleh <?=$row['authorName']?>: <?=DateTimeToDate($row['postDate'],'-')?></span><br>
+						<div style="height:5px"></div>
+				<?php
+					}
+				?>							
+				<a class="titleTextRight" style="padding-left:90px" href="index.php?g=book_list" title="Lihat semua buku">Lihat Semua Buku >></a>										
+		</div>-->
+		</br>
+		  <img src="images/counter.jpg" width="241" border="none" /><br>
+		<div style="width:200px;height:40px;">
+		 <div style="width:75px;height:40px;float:left">
+		 </div>
+		 <div style="float:left;width:100px;font-weight:bold; height:40px;background-color:#000;padding-top:2px" align="center" id="counter"> <strong>
+            <font color="#2c7ef9" size="6" face="Book Antiqua">
+			<?php
+				// This code must be inserted somewhere in your index page to display the hits counter
+					include_once ("./hitcounter/check.php");
+					echo $data;
+			?>
+			 </font> </strong>
+		  	</div>
+		 </div>
+		 <div class="clearboth"></div>
+		 <!-- <a href="http://www.digits.com" target="_blank">				
+				<img src="http://counter.digits.com/?counter={e5ab8edb-5466-14e4-e90a-f9d20c09acb1}&template=hitcounter/wc.gif" 
+				 alt="Hit Counter by Digits" border="0"  />
+			  </a-->
+			  <!-- Start of Globel Code -->
+				<!--<CENTER>
+				<script language="JavaScript">
+				var count = "perkantasjatim1";          // Change Your Account?
+				var type = "cold";       // Change Your Counter Image?
+				var digits = "5";          // Change The Amount of Digits on Your Counter?
+				var prog = "unique";          // Change to Either hit/unique?
+				var statslink = "yes";    // provide statistical link in counter yes/no?
+				var sitelink = "yes";     // provide link back to our site;~) yes/no?
+				var cntvisible = "yes"; // do you want counter visible yes/no?
+				</script>
+				<script language="JavaScript" src="http://006.free-counters.co.uk/count-002.js">
+				</script>
+				<noscript>
+				<a href="http://www.free-counters.co.uk/" target="_blank">
+				<img  src="http://006.free-counters.co.uk/count-002.pl?count=perkantasjatim1&cntvisible=no&mode=noscript" alt="Free Counters" title="Free Counters" border="0">
+				</a>The following text will not be seen after you upload your website,
+				please keep it in order to retain your counter functionality 
+				<br>The UK is regulating online casino gambling.  simply by putting attention to website offerring <a href="http://www.internet-gambling.com" target="_blank">internet gambling</a><br> the purchace process can be optimized. Stay home and do your gambling in an online casino. <a href="http://www.free-counters.co.uk/" target="_blank">counter</a><br>
+				-->
+				</noscript>
+				<!-- END DO NOT TAMPER WITH ANYTHING ELSE ABOVE THIS LINE FOR YOUR WEBTV & UNIX VISITORS -->
+				</CENTER>        
+				<!-- End of Globel Code -->
+
+		  
+		 <div align="center" id="counter" style="padding-left:10px">
+            <font color="white" size="1" >
+        <?php
+			$onlineUser = $user->checkUserLogin();
+			$onlineGuest = $user->checkGuestOnline();
+					echo "<br>Saat ini ada $onlineGuest tamu dan $NUM_USER_ONLINE online user <br>";
+			if ($NUM_USER_ONLINE > 0){
+		?>
+          </font>
+
+		 </div>
+		 <div align="center" id="counter">
+		 <font color="white" size="1" >
+		 <strong>ONLINE USER :</strong><br/>
+		 </font>
+         <font color="blue" size="1" >
+        <?php
+				foreach ($onlineUser as $ou)
+				{
+					echo "<a href=\"index.php?g=user&id=".$ou['user_id']."\">".$ou['name']."</a> <br>";
+				}
+			}
+        ?>
+          </font>
+		 </div>
+		<div class="contentRight-2">
+<!-- Begin ShoutMix - http://www.shoutmix.com -->
+<iframe title="perkantasjatim" src="http://www6.shoutmix.com/?perkantasjatim" width="220" height="370" frameborder="0" scrolling="auto">
+<a href="http://www6.shoutmix.com/?perkantasjatim">View shoutbox</a>
+</iframe>
+<!--<br /><a href="http://www.shoutmix.com" title="Get your own free shoutbox chat widget at ShoutMix!">ShoutMix chat widget</a><br />-->
+<!-- End ShoutMix -->
+
+        </div>
+		<div class="header_menukanan"><img src="images/newcomment.jpg" width="241" height="30" alt="KOMENTAR TERBARU" />
+        </div>
+		<div style="text-align:left; padding-left:10px">
+		
+			<font color="black" size="2">
+			<?
+			$cl = $comment->getHomeListComment();
+			Foreach($cl as $row)
+			{
+				echo "<div style=\"padding-top:5px\">";
+				echo "<strong><font>";
+				echo "<a class='titleTextRight' title='".$row['title']."' href=\"index.php?g=".$row['commentFor']."&id=".$row['commentForId']."#comment-".$row['id']."\">".substr($row['title'],0,28)."..."."</a><br>";
+				echo "</font></strong><font size=\"1\">";
+				echo "&nbsp;&nbsp;&nbsp;<a style='color:blue' href=\"index.php?g=user&id=".$row['user_id']."\">".$row['name']."</a>, ";
+				echo "".$row['postDate']."<br>";
+				echo "</font>";
+				echo "</div>";
+			}
+			?>
+			</font><br>
+        </div>
+		<!--<script type="text/javascript" src="http://feedjit.com/serve/?vv=693&amp;tft=3&amp;dd=0&amp;wid=9e1ddd1186cfec2c&amp;pid=0&amp;proid=0&amp;bc=FFFFFF&amp;tc=000000&amp;brd1=012B6B&amp;lnk=135D9E&amp;hc=FFFFFF&amp;hfc=2853A8&amp;btn=C99700&amp;ww=241&amp;wne=5&amp;wh=Live+Traffic+Feed&amp;hl=0&amp;hlnks=0&amp;hfce=0&amp;srefs=0&amp;hbars=0"></script><noscript><a href="http://feedjit.com/">Feedjit Live Blog Stats</a></noscript>
+	-->
+	</div>
+</td>  
