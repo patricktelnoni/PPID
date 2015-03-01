570397a8d59a0331c9dc1f623897fdae560c6e41
Patrick Telnoni <ptelnoni89@gmail.com>
2015-03-01 14:14:16 +0700

    Halaman Registrasi

---
+++ application/views/registration.php
@@ -0,0 +1,78 @@
+<div class="contentMain">		
+				<div class="slideshow">
+					
+					<div>
+	<table class="contentEdit" align="left" width="100%" cellpadding="0" cellspacing="0" border="0">
+		<tr>
+			<td align="left"  class="titleContentCenter" style=" width:100%;padding:0 0 0 5px;font-size:18px;">
+			</td>	
+		</tr>
+		<tr>
+			<td style="padding:10px 10px 0 10px">
+				<form name="register" method="post" action="./c_registration/signup" enctype="multipart/form-data">
+				<input type="hidden" name="user_id" id="user_id" value = "">
+				<BR/>
+				<table border="0" cellpadding="5" cellspacing="0">
+					<tr>
+						<td align="left"  colspan="2" class="titleContentCenter" style=" width:100%;padding:0 0 0 5px;font-size:16px;">INFORMASI LOGIN</td>	
+					</tr>
+					<tr>
+						<td class="main">Email (sebagai username)*:</td>
+						<td><input type="text" id="email" name="email" value=""  size="30" class="textbox"></td>
+					</tr>
+					<tr>
+						<td class="main">Password*:</td>
+						<td><input type="password" id="password" name="password" value=""  size="30" class="textbox"></td>
+					</tr>
+					<tr>
+						<td class="main">Ulangi Password*:</td>
+						<td><input type="password" id="password2" name="password2" value=""  size="30" class="textbox"></td>
+					</tr>
+						
+					<tr>
+						<td align="left"  colspan="2" class="titleContentCenter" style=" width:100%;padding:0 0 0 5px;font-size:14px;">DATA USER</td>	
+					</tr>
+					<tr>
+						<td class="main">Nama Lengkap*:</td>
+						<td><input type="text" id="name" name="name" value=""  size="30" class="textbox"></td>
+					</tr>
+										
+					<tr>
+						<td class="main">Alamat*:</td>
+						<td><input type="text" id="address" name="address" value=""  size="30" class="textbox"></td>
+					</tr>
+					<tr>
+						<td class="main">Kota*:</td>
+						<td><input type="text" id="city" name="city" value=""  size="30" class="textbox"></td>
+					</tr>
+					<tr>
+						<td class="main">Propinsi*:</td>
+						<td><input type="text" id="province" name="province" value=""  size="30" class="textbox"></td>
+					</tr>
+					<tr>
+						<td class="main">Kode Pos:</td>
+						<td><input type="text" id="postalcode" name="postalcode" value=""  size="10" class="textbox"></td>
+					</tr>
+					<tr>
+						<td class="main">No.Telp:</td>
+						<td><input type="text" id="phone" name="phone" value=""  size="30" class="textbox"></td>
+					</tr>
+					
+					<tr>
+						<td valign="top"></td>
+						<td><input type="submit" id="btnSubmit" name="submitthisform"  value="Submit"  class="button"> &nbsp;
+						<input type="button" style="cursor:pointer" onClick="history.back()" value="Kembali" class="button">
+						</td>
+					</tr>
+					
+				</table>
+				</form>
+			</td>
+		</tr>
+	</table>
+					</div>
+					
+				 </div>
+				
+			</div>
+
