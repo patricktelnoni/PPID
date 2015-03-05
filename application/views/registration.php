d2834c0170b9e920032fe43e6a3ccea120227304
Patrick Telnoni <ptelnoni89@gmail.com>
2015-03-05 15:50:03 +0700

    Ganti layout form registrasi

--- application/views/registration.php
+++ application/views/registration.php
@@ -3,70 +3,70 @@
 					
 					<div>
 	<table class="contentEdit" align="left" width="100%" cellpadding="0" cellspacing="0" border="0">
-		<tr>
-			<td align="left"  class="titleContentCenter" style=" width:100%;padding:0 0 0 5px;font-size:18px;">
-			</td>	
-		</tr>
+		
 		<tr>
 			<td style="padding:10px 10px 0 10px">
-				<form name="register" method="post" action="./c_registration/signup" enctype="multipart/form-data">
-				<input type="hidden" name="user_id" id="user_id" value = "">
-				<BR/>
-				<table border="0" cellpadding="5" cellspacing="0">
-					<tr>
+			<form class="form-horizontal" role="form" method="post"  action="./c_registration/signup" enctype="multipart/form-data">
+      <div class="form-group">
+	<table style="margin-left:5%;">
+	<tr>
 						<td align="left"  colspan="2" class="titleContentCenter" style=" width:100%;padding:0 0 0 5px;font-size:16px;">INFORMASI LOGIN</td>	
 					</tr>
-					<tr>
-						<td class="main">Email (sebagai username)*:</td>
-						<td><input type="text" id="email" name="email" value=""  size="30" class="textbox"></td>
-					</tr>
-					<tr>
-						<td class="main">Password*:</td>
-						<td><input type="password" id="password" name="password" value=""  size="30" class="textbox"></td>
-					</tr>
-					<tr>
-						<td class="main">Ulangi Password*:</td>
-						<td><input type="password" id="password2" name="password2" value=""  size="30" class="textbox"></td>
-					</tr>
-						
-					<tr>
-						<td align="left"  colspan="2" class="titleContentCenter" style=" width:100%;padding:0 0 0 5px;font-size:14px;">DATA USER</td>	
-					</tr>
-					<tr>
-						<td class="main">Nama Lengkap*:</td>
-						<td><input type="text" id="name" name="name" value=""  size="30" class="textbox"></td>
-					</tr>
-										
-					<tr>
-						<td class="main">Alamat*:</td>
-						<td><input type="text" id="address" name="address" value=""  size="30" class="textbox"></td>
-					</tr>
-					<tr>
-						<td class="main">Kota*:</td>
-						<td><input type="text" id="city" name="city" value=""  size="30" class="textbox"></td>
-					</tr>
-					<tr>
-						<td class="main">Propinsi*:</td>
-						<td><input type="text" id="province" name="province" value=""  size="30" class="textbox"></td>
-					</tr>
-					<tr>
-						<td class="main">Kode Pos:</td>
-						<td><input type="text" id="postalcode" name="postalcode" value=""  size="10" class="textbox"></td>
-					</tr>
-					<tr>
-						<td class="main">No.Telp:</td>
-						<td><input type="text" id="phone" name="phone" value=""  size="30" class="textbox"></td>
-					</tr>
-					
-					<tr>
-						<td valign="top"></td>
-						<td><input type="submit" id="btnSubmit" name="submitthisform"  value="Submit"  class="button"> &nbsp;
-						<input type="button" style="cursor:pointer" onClick="history.back()" value="Kembali" class="button">
-						</td>
-					</tr>
-					
-				</table>
-				</form>
+	<tr>
+          <td><label class="col-md-8 control-label">Email</label></td>
+          <td><div class="col-md-24">
+               <input type="text" ng-model="name"  class="form-control" value=""/>
+           
+          <br></div></td>
+	</tr>
+
+	<tr>
+          <td><label class="col-md-8 control-label">Password</label></td>
+          <td><div class="col-md-20">
+               <input type="text" ng-model="name"  class="form-control" value=""/><br>
+           
+          </div></td>
+	</tr>
+
+	<tr>
+		<td align="left"  colspan="2" class="titleContentCenter" style=" width:100%;padding:0 0 0 5px;font-size:14px;">DATA USER</td>	
+	</tr>
+	<tr>
+          <td><label class="col-md-8 control-label">Nama </label></td>
+          <td><div class="col-md-20">
+               <input type="text" ng-model="name"  class="form-control" value=""/><br>
+           
+          </div></td>
+	</tr>
+	<tr>
+          <td><label class="col-md-8 control-label">Alamat</label></td>
+          <td><div class="col-md-20">
+               <input type="text" ng-model="name"  class="form-control" value=""/><br>
+           
+          </div></td>
+	</tr>
+	<tr>
+          <td><label class="col-md-8 control-label">Kota</label></td>
+          <td><div class="col-md-20">
+               <input type="text" ng-model="name"  class="form-control" value=""/><br>
+           
+          </div></td>
+	</tr>
+	<tr>
+          <td><label class="col-md-8 control-label">Propinsi</label></td>
+          <td><div class="col-md-20">
+               <input type="text" ng-model="name"  class="form-control" value=""/><br>
+           
+          </div></td>
+	</tr>
+	<tr>
+          <td colspan="2"><label class="col-md-8 control-label"><button type="submit" class="btn btn-success">Daftar</button></label></td>
+    
+	</tr>
+	</table>
+	
+      </div>
+    </form>			
 			</td>
 		</tr>
 	</table>
